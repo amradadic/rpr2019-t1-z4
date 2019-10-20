a736cabe312423a24761be00f14005d157f5a068
adadic1 <44211420+adadic1@users.noreply.github.com>
2019-10-20 21:11:23 +0200

    dovrsiti

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String naziv;
+    private double cijena;
+    private int kod;
+
+    public Artikl() {
+    }
+
+    public Artikl(String naziv, double cijena, int kod) {
+        this.naziv = naziv;
+        this.cijena = cijena;
+        this.kod = kod;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    public int getKod() {
+        return kod;
+    }
+
+    public void setKod(int kod) {
+        this.kod = kod;
+    }
 }
--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Korpa {
+    ArrayList<Artikl> artikli = new ArrayList<>();
+    int brojac = 0;
+    public Korpa(ArrayList<Artikl> artikli) {
+        this.artikli = artikli;
+    }
+
+    public Korpa() {
+    }
 }
